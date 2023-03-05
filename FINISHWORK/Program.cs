e68523cc6e954e73476d85ced6667f2554f85c2a
Formanty <tommyformant@yandex.ru>
2023-03-05 12:57:52 +0300

    Added a method to check strings and create a new array

--- FINISHWORK/Program.cs
+++ FINISHWORK/Program.cs
@@ -1,1 +1,15 @@
 ﻿string[] myarray = {"Hello", "=)", "567", "formant", "8", "some"} ;
+string[] newarray = new string[myarray.Length];
+
+void CheckSymbols(string[] myarray)
+{
+    int count = 0;
+    for(int i = 0; i < myarray.Length; i++)
+    {
+       if(myarray[i].Length <= 3)
+       {
+        newarray[count] = myarray[i];
+        count++;
+       }
+    }
+}
